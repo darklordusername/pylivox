12db5e9c0f9d0408256e1f882f29d27aff7abbee
alexey <stm32f103@mail.ru>
2022-11-20 17:49:44 +0000

    U Added empty tests. req.txt

---
+++ tests/test_general.py
@@ -0,0 +1,71 @@
+import pytest
+
+
+def test_BroadcastMsg():
+    raise NotImplementedError   
+
+def test_Handshake():
+    raise NotImplementedError
+
+def test_HandshakeResponse():
+    raise NotImplementedError
+
+def test_QueryDeviceInformation():
+    raise NotImplementedError
+
+def test_QueryDeviceInformationResponse():
+    raise NotImplementedError
+
+def test_HeartbeatResponse():
+    raise NotImplementedError
+
+def test_StartStopSampling():
+    raise NotImplementedError
+
+def test_StartStopSamplingResponse():
+    raise NotImplementedError
+
+def test_ChangeCoordinateSystem():
+    raise NotImplementedError
+
+def test_ChangeCoordinateSystemResponse():
+    raise NotImplementedError
+
+def test_Disconnect( ):
+    raise NotImplementedError
+
+def test_DisconnectResponse( ):
+    raise NotImplementedError
+
+def test_PushAbnormalStatusInformation( ):
+    raise NotImplementedError
+
+def test_ConfigureStaticDynamicIp( ):
+    raise NotImplementedError
+
+def test_ConfigureStaticDynamicIpResponse( ):
+    raise NotImplementedError
+
+def test_ConfigureStaticDynamicIpResponse( ):
+    raise NotImplementedError
+
+def test_GetDeviceIpInformation( ):
+    raise NotImplementedError
+
+def test_GetDeviceIpInformationResponse( ):
+    raise NotImplementedError
+
+def test_RebootDevice( ):
+    raise NotImplementedError
+
+def test_RebootDeviceResponse( ): 
+    raise NotImplementedError
+
+def test_WriteConfigurationParameters( ):
+    raise NotImplementedError
+
+def test_WriteConfigurationParametersResponse( ):
+    raise NotImplementedError
+
+def test_ReadConfigurationParameters( ):
+    raise NotImplementedError
