cbf22180f0801da5c468aa64b009463143c87379
Kulkov Alexey QT <stm32f103@mail.ru>
2023-02-01 21:05:12 +0300

    U moved from main to lidar emulator logic.

--- main.py
+++ main.py
@@ -7,42 +7,10 @@ from binascii import a2b_hex
 import log
 from pylivox.control import general, lidar
 from pylivox.control.utils import FrameFrom
+from pylivox.lidar import Lidar
 
 logger = log.getLogger(__name__)
 logger.info('========== START ==========')
 
 if __name__ == '__main__':
-    broadcast = general.BroadcastMsg(
-            general.Broadcast( 'test', 0), 
-            general.DeviceType.Mid40)
-    broadcast_msg = broadcast.frame
-    seq = 0
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.bind((str(ipaddress.IPv4Address("0.0.0.0")), 65000)) 
-    address = ipaddress.IPv4Address('255.255.255.255')
-    port = 55000
-    while True:
-        logger.debug('broadcast...')
-        s.settimeout(0.1)
-        s.sendto(broadcast_msg, (str(address), port) )
-        try:
-            data, addr = s.recvfrom(1024)
-            handshake = FrameFrom(data)
-            logger.debug(handshake)
-            if type(handshake) is not general.Handshake:
-                raise TypeError
-            # s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 0)
-            akn = general.HandshakeResponse(False).frame
-            s.sendto(akn, (str(handshake.ip), handshake.cmd_port))
-            logger.debug('>> akn')
-            while True:
-                s.settimeout(5)
-                data, addr = s.recvfrom(1024)
-                f = FrameFrom(data)
-                logger.debug(f)
-        except socket.timeout:
-            pass
-        except Exception as e:
-            logger.exception(e)
-        time.sleep(1)
+   Lidar('test')
--- pylivox/lidar.py
+++ pylivox/lidar.py
@@ -1,19 +1,113 @@
-
 #std
-import ipaddress
+import threading
+import time
 import socket
-#libs
-#proj
+import ipaddress
+#import proj
+import log
+from pylivox.control import general, lidar
 from pylivox.control.frame import Frame
+from pylivox.control.utils import FrameFrom
+
+logger = log.getLogger(__name__)
+
 
 class Lidar:
 
-    def __init__(self):
+    def __init__(self, serial:'str|bytes'):
+        self.master:general.Handshake = None
+        self.heartbeat_time = time.time() - 5
+        self.is_connected = False
+        self.state:general.WorkState.Lidar = general.WorkState.Lidar.Standby
+        self.serial = serial
         self.seq = 0
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self._socket.bind((str(ipaddress.IPv4Address("0.0.0.0")), 65000))
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.s.bind((str(ipaddress.IPv4Address("0.0.0.0")), 65000)) 
+        self._broadcast()
+        self._rx()
 
-    def send(self, cmd:Frame, address:ipaddress.IPv4Address, port:int):
-        self.seq += 1
-        self._socket.sendto(cmd.frame, (str(address), port) )
+    def _broadcast(self):
+        def t():
+            while True:
+                try:
+                    time.sleep(1)
+                    elapsed = time.time() - self.heartbeat_time
+                    if elapsed < 5:
+                        continue
+                    self.is_connected = False
+                    logger.debug('broadcast...')
+                    broadcast = general.BroadcastMsg(
+                            general.Broadcast( self.serial, 0), 
+                            general.DeviceType.Mid40).frame
+                    self.s.sendto(broadcast, ('255.255.255.255', 55000) )
+                except Exception as e:
+                    logger.exception(e)
+        self._broadcast_thread = threading.Thread(target=t, name='broadcast', daemon=True)
+        self._broadcast_thread.start()
+    
+    def _rx(self):
+        while True:
+            try:
+                data, addr = self.s.recvfrom(1024*2)
+                self.heartbeat_time = time.time()
+                frame = FrameFrom(data)
+                logger.info(f'<< {addr} {frame}')
+                handler = self.HANDLERS[type(frame)]
+                handler(self, frame)
+            except socket.timeout:
+                pass
+            except KeyError as e:
+                logger.warning(f'Unknown frame {data} cmd_set:{data[-6]} cmd_id:{data[-5]}')
+            except Exception as e:
+                logger.exception(e)
+            time.sleep(1)
+
+    def send(self, frame:Frame):
+        self.s.sendto(frame.frame, (str(self.master.ip), self.master.cmd_port))
+
+    def onHandshake(self, handshake:general.Handshake):
+        if not self.is_connected:
+            self.master = handshake
+            akn = general.HandshakeResponse(False)
+            self.heartbeat_time = time.time()
+            self.is_connected = True
+            self.send(akn)
+
+    def onHeartbeat(self, heartbeat:general.Heartbeat):
+        # self.heartbeat_time = time.time()   
+        akn = general.HeartbeatResponse(is_error=False, 
+                                        work_state=self.state, 
+                                        feature_msg=0,
+                                        ack_msg=0)
+        self.send(akn)
+
+    def onDisconnect(self, disconnect:general.Disconnect):
+        pass
+
+    def onQueryDeviceInformation(self, info:general.QueryDeviceInformation):
+        pass
+
+    def onReadLidarExtrinsicParameters(self, req:lidar.ReadLidarExtrinsicParameters):
+        pass
+
+    def onGetDeviceIpInformation(self, ip_info:general.GetDeviceIpInformation):
+        pass
+
+    def onGetTurnOnOffFanState(self, req:lidar.GetTurnOnOffFanState):
+        pass
+
+    def onGetImuDataPushFrequency(self, req:lidar.GetImuDataPushFrequency):
+        pass
+
+
+    HANDLERS = {
+        general.Handshake                     : onHandshake                    ,                                        
+        general.Heartbeat                     : onHeartbeat                    ,                
+        general.Disconnect                    : onDisconnect                   ,                                    
+        general.QueryDeviceInformation        : onQueryDeviceInformation       ,                                    
+        lidar.ReadLidarExtrinsicParameters    : onReadLidarExtrinsicParameters ,                                    
+        general.GetDeviceIpInformation        : onGetDeviceIpInformation       ,                                    
+        lidar.GetTurnOnOffFanState            : onGetTurnOnOffFanState         ,                                    
+        lidar.GetImuDataPushFrequency         : onGetImuDataPushFrequency      ,                                    
+    }
